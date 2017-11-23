ba7529d6a376a1bbb0100cc650347c0e985f6049
vcharkashyn <vcharkashyn@gmail.com>
2017-11-24 01:44:09 +0300

    Spring DI was added

--- src/java/org/example/apps/LoginManager.java
+++ src/java/org/example/apps/LoginManager.java
@@ -4,16 +4,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LoginManager {
+    private Stage stage;
 
-    private final Stage stage;
-
-    public LoginManager(Stage primaryStage) {
+    public void initAuthScreen(Stage primaryStage) throws Exception {
         stage = primaryStage;
-    }
-
-    public void initAuthScreen() throws Exception {
         Parent root = FXMLLoader.load(LoginManager.class.getResource("/fxml/authWindow.fxml"));
 
         stage.setTitle("Authorization window");
--- src/java/org/example/apps/startup/SmartPurseLauncher.java
+++ src/java/org/example/apps/startup/SmartPurseLauncher.java
@@ -6,18 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.example.apps.LoginManager;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SmartPurseLauncher extends Application {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"SpringBeans.xml"});
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-       LoginManager loginManager = new LoginManager(primaryStage);
-
+    public void start(Stage primaryStage) throws Exception {
+        ApplicationContext context = new AnnotationConfigApplicationContext(org.example.config.ApplicationContext.class);
+        LoginManager loginManager = context.getBean(LoginManager.class);
+        loginManager.initAuthScreen(primaryStage);
     }
 
 }
